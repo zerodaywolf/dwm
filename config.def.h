74c689ccdc201af517f6cb117dd8224233b76fe8
vulf <pranavsivvam@gmail.com>
2020-07-12 21:30:11 +0530

    modified colors

--- config.def.h
+++ config.def.h
@@ -8,14 +8,15 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#F3F6F9"; // white
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray5[]       = "#005577"; // default dwm blue
+static const char col_cyan[]        = "#439DB6"; // light blue
 static const char *colors[][4]      = {
 	/*               fg         bg         border     float */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2, col_gray4 },
-	[SchemeSel] =  { col_gray4, col_cyan,  col_cyan, col_cyan },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan, col_gray4 },
+	[SchemeSel] =  { col_gray4, col_cyan,  col_gray2, col_cyan },
 };
 
 /* tagging */
